5ae9df7c3997bade2f7ff3a9acfe239dd5c67bda
Muhammad <m.tareq@live.ca>
2019-01-16 12:33:54 -0500

    test1

---
+++ Project/testProject1/src/testProject1/testmain.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package testProject1;
+
+/**
+ * @author TZ-L
+ *
+ */
+public class testmain {
+
+	/**
+	 * 
+	 */
+	public testmain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
