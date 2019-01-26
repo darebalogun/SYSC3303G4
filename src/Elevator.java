082e62d94ac676a37913db7239263d93ae90e3c8
Muhammad Tarequzzaman <m.tareq@live.ca>
2019-01-26 14:21:17 -0500

    Elevator init

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,40 +1,31 @@
 import java.util.*;
 
 public class Elevator {
-	
-	public ArrayList<Boolean> buttonList; 
-	public ArrayList<Boolean> elevatorLamp; 
+
+	public ArrayList<Boolean> buttonList;
+	public ArrayList<Boolean> elevatorLamp;
 	public int timeBtwFloors;
 	public int doorDelay;
 	public Boolean dooropen;
-	public int currentFloor; 
+	public int currentFloor;
 	public int nextFloor;
-	
-	
-	
-	
-
-
-
 
 	public Elevator(int timeBtwFloors, int doorDelay, Boolean dooropen, int currentFloor, int nextFloor) {
-		
-		// create ButtonList for 13 floor and Initialize as FALSE 
+
+		// create buttonList for 13 floor and Initialize as FALSE
 		this.buttonList = new ArrayList<Boolean>(Arrays.asList(new Boolean[13]));
 		Collections.fill(buttonList, Boolean.FALSE);
-		
-		// create Elevatorlamp for 13 floor and Initialize as FALSE 
+
+		// create elevatorLamp for 13 floor and Initialize as FALSE
 		this.elevatorLamp = new ArrayList<Boolean>(Arrays.asList(new Boolean[13]));
 		Collections.fill(buttonList, Boolean.FALSE);
-		
-		this.timeBtwFloors = timeBtwFloors;
-		
-		this.doorDelay = doorDelay;
-		
-		this.dooropen = false;
-		
-		
-	}
-	
-}
 
+		this.timeBtwFloors = timeBtwFloors;
+
+		this.doorDelay = doorDelay;
+
+		this.dooropen = false;
+
+	}
+
+}
