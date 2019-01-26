028cc5f64c4fb5d5bcd339c85ceaac1b4c201210
samaadilsheikh <46900451+samaadilsheikh@users.noreply.github.com>
2019-01-26 14:18:15 -0500

    Made changes to floor

--- src/FloorSubsystem.java
+++ src/FloorSubsystem.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
@@ -14,8 +13,7 @@ public class FloorSubsystem {
 	private static final int FLOOR_COUNT = 1;
 	
 	// Datagram sockets used to send and receive packets to the Scheduler
-	private DatagramSocket sendReceiveSocket;
-	private DatagramPacket sendPacket, receivePacket;
+	private DatagramSocket sendReceive;
 	
 	// SEND_PORT is the port on the scheduler where data is sent and RECIEVE_PORT is where the floor subsystem listens for incoming data 
 	private static final int SEND_PORT = 5000, RECEIVE_PORT = 5001;
@@ -23,29 +21,21 @@ public class FloorSubsystem {
 	// Text file containing events to be sent to scheduler
 	private static final String = "/Inp"
 	
-	public FloorSubsystem() 
-	{
+	public FloorSubsystem() {
 		this.floors = new ArrayList<Floor>(FLOOR_COUNT);
-
-		try
-		{
-			sendReceiveSocket = new DatagramSocket();
-		}
-		catch(SocketException se)
-		{
-			se.printStackTrace();
-			System.exit(1);
-		}
+		
+		try {
+	         // Create send socket and bind it to the 
+	         this.sendReceive = new DatagramSocket();
+	      } catch (SocketException se) {   // Can't create the socket.
+	         se.printStackTrace();
+	         System.exit(1);
+	      }
 		
 		
 	}
 	
 	public Event readInputEvent(File: file) {
-		
-	}
-	
-	public void sendAndReceiveFromScheduler()
-	{
 		
 	}
 	
