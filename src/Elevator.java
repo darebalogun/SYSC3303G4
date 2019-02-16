1b62a2de1ff70c010c35d7852a90337f2b354010
anannyabhatia <43757391+anannyabhatia@users.noreply.github.com>
2019-02-16 11:36:44 -0500

    Update Elevator.java

--- src/Elevator.java
+++ src/Elevator.java
@@ -58,7 +58,7 @@ public class Elevator extends Thread {
 	 */
 	public Elevator(int elevatorNumber, int numberofFloorbuttons, int RECEIVE_PORT, int startFloor) {
 
-		System.out.println("ElevatorSubSystem running...Waiting for the requests from the Scheduler \n");
+		//System.out.println("ElevatorSubSystem running...Waiting for the requests from the Scheduler \n");
 
 		// create buttonList for buttons floor and Initialize as FALSE
 		buttonList = new ArrayList<>(Arrays.asList(new Boolean[numberofFloorbuttons]));
