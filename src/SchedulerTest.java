c390e786cd756fe0061d52c139619036be70c294
Mrunal <mrunal.patel1997@gmail.com>
2019-02-16 12:32:40 -0500

    sdad

---
+++ src/SchedulerTest.java
@@ -0,0 +1,16 @@
+import org.junit.*;
+
+public class SchedulerTest {
+
+	Scheduler stest = new Scheduler();
+	
+	
+	public static void setupBeforeClass() throws Exception{
+		
+	}
+	
+	public static void tearDownAfterClass() throws Exception{
+		
+	}
+	
+}
